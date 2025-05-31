dbf365773d3d5c563c1b0bfb7388ad2f8fd68444
dmknpd <soupuabt@gmail.com>
2025-05-31 17:00:52 +0300

    added searchChat

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -7,5 +7,6 @@ router.get("/chats", chatController.getAllChats);
 router.post("/chats", chatController.createChat);
 router.patch("/chats/:id", chatController.updateChat);
 router.delete("/chats/:id", chatController.deleteChat);
+router.get("/chats/search", chatController.searchChat);
 
 module.exports = router;
