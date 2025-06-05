8fee81d82b71bd01c78153de9a19d60ed3d8ec3c
dmknpd <soupuabt@gmail.com>
2025-06-05 20:49:49 +0300

    deploy

--- backend/index.js
+++ backend/index.js
@@ -59,10 +59,4 @@ io.on("connection", (socket) => {
   });
 });
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-app.get("*", (_, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"));
-});
-
 server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
